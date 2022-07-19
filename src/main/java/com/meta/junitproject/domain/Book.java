880638788c83e839427b8a2453ba22196f734d37
HYUNBIN_CHO <bluesky3268@gmail.com>
2022-07-20 01:22:28 +0900

    Book 엔티티

--- src/main/java/com/meta/junitproject/domain/Book.java
+++ src/main/java/com/meta/junitproject/domain/Book.java
@@ -1,4 +1,33 @@
 package com.meta.junitproject.domain;
 
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class Book {
+
+    @Id
+    private long id;
+
+    @Column(length = 50, nullable = true)
+    private String title;
+
+    @Column(length = 20, nullable = true)
+    private String author;
+
+    @Builder
+    public Book(long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
 }
